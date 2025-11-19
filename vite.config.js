62d87a2264b8c2fc0e39f2b6ca10438032fb62ce
porames.at <porames.at@logicspark.com>
2025-11-19 09:55:04 +0700

    Remove unused React plugin import from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [tailwindcss(),reactRouter()],
